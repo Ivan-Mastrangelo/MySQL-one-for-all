ef71962d060147f46fd219b9019bb170016d4d58
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-03-11 23:48:06 -0300

    Corrige requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -2,4 +2,6 @@ SELECT m.nome AS cancao, COUNT(hr.musica_id) AS reproducoes
 FROM musicas AS m
 INNER JOIN historico_de_reproducoes AS hr ON m.id = hr.musica_id
 GROUP BY m.nome
-ORDER BY reproducoes DESC, m.nome;
+ORDER BY reproducoes DESC, m.nome
+LIMIT 2;
+
