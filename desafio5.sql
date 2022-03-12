3dccbd35456ce457e00486f0e8e162aa6039c9bb
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-03-11 23:39:28 -0300

    Add requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,5 @@
+SELECT m.nome AS cancao, COUNT(hr.musica_id) AS reproducoes
+FROM musicas AS m
+INNER JOIN historico_de_reproducoes AS hr ON m.id = hr.musica_id
+GROUP BY m.nome
+ORDER BY reproducoes DESC, m.nome;
